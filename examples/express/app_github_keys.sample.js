da859278d6d998d14f814ddf301dbbfad79a5b40
Jerome Etienne <jerome.etienne@gmail.com>
2015-01-26 22:54:01 +0100

    more work

--- examples/express/app_github_keys.sample.js
+++ examples/express/app_github_keys.sample.js
@@ -1,5 +1,5 @@
 module.exports = {
-	// keys from github app
+	// keys from github app 
 	// - see https://github.com/settings/applications and click 'register new application'
 	clientID	: 'PUTYOUROWNHERE',
 	clientSecret	: 'PUTYOUROWNHERE',
