a422f561a050ede03ce9cf3f3e01ad976dc6d510
Jerome Etienne <jerome.etienne@gmail.com>
2015-01-26 22:54:40 +0100

    removed

--- examples/express/app_github_key.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	// keys from github app
-	// - see https://github.com/settings/applications and click 'register new application'
-	clientID	: 'ea5914292ffcf9cab776',
-	clientSecret	: 'db108ec2e53a02bda9a7162548b10587986ca9c4',
-}
